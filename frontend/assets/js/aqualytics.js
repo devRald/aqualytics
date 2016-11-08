711c6be22954cecd94888bfdeff5f0ef6a514a99
devrald24 <geraldmichaelaustria@gmail.com>
2016-11-09 00:57:22 +0800

    fix size of line graph

--- frontend/assets/js/aqualytics.js
+++ frontend/assets/js/aqualytics.js
@@ -3,66 +3,24 @@
 			return fn;
 		}
 
-		fn.getData = function(){
-			fn.initGraph();
+		fn.getData = function(data){
+			fn.initGraph(data);
 		}
 
-		fn.initGraph = function(){
-			var data = {
-				high:3,
-				low:0,
-			  labels: ['1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00', '8:00', '9:00', '10:00', '11:00', '12:00'],
-			  series: [
-			    {
-			      data: [1, 2, 3, 5, 8, 13, 4, 6, 8, 12, 11, 5]
-			    }
-			  ]
-			};
-
-			/* Set some base options (settings will override the default settings in Chartist.js *see default settings*). We are adding a basic label interpolation function for the xAxis labels. */
-			var options = {
-			  axisX: {
-			    labelInterpolationFnc: function(value) {
-			      return value;
-			    }
-			  },
-			  fullWidth:true
-			};
-
-			/* Now we can specify multiple responsive settings that will override the base settings based on order and if the media queries match. In this example we are changing the visibility of dots and lines as well as use different label interpolations for space reasons. */
-			var responsiveOptions = [
-			  ['screen and (min-width: 641px) and (max-width: 1024px)', {
-			    showPoint: false,
-			    axisX: {
-			      labelInterpolationFnc: function(value) {
-			        return 'Week ' + value;
-			      }
-			    }
-			  }],
-			  ['screen and (max-width: 640px)', {
-			    showLine: false,
-			    axisX: {
-			      labelInterpolationFnc: function(value) {
-			        return 'W' + value;
-			      }
-			    }
-			  }]
-			];
-
+		fn.initGraph = function(data){
 			/* Initialize the chart with the above settings */
-			new Chartist.Line('#myChart', {
-			  labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-			  series: [
-			    [100, 9, 7, 8, 5],
-			    [2, 1, 3.5, 7, 3],
-			    [1, 3, 4, 5, 6]
-			  ]
-			}, {
-				high:100,
+			new Chartist.Line('.ct-chart', data, 
+			{
+				high:14,
+				low: 0,
+				divisor:10,
+				height: '300px',
 			  fullWidth: true,
 			  chartPadding: {
-			    right: 3
-			  }
+			    right: 30
+			  },lineSmooth: Chartist.Interpolation.cardinal({
+			    tension: 10
+			  })
 			});
 		}
 
