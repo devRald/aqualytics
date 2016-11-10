0e880121cc662877f754366f48599779fe09b9bd
devrald24 <geraldmichaelaustria@gmail.com>
2016-11-11 01:30:20 +0800

    updated the dashboard element placing

--- frontend/assets/js/aqualytics.js
+++ frontend/assets/js/aqualytics.js
@@ -6,6 +6,7 @@
 		fn.getData = function(){
 			fn.initLine();
 			fn.initDoughnut();
+			fn.initTemp()
 		}
 
 		fn.initDoughnut = function(){
@@ -17,16 +18,23 @@
 		    ],
 		    datasets: [
 		        {
-		            data: [300, 50, 100],
+		            data: [13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13],
 		            backgroundColor: [
-		                "#FF6384",
-		                "#36A2EB",
-		                "#FFCE56"
-		            ],
-		            hoverBackgroundColor: [
-		                "#FF6384",
-		                "#36A2EB",
-		                "#FFCE56"
+		                "#ee1c25",
+		                "#f26724",
+		                "#f8c611",
+		                "#f5ed1c",
+		                "#b5d333",
+		                "#84c341",
+		                "#4db749",
+		                "#33a94b",
+		                "#22b46b",
+		                "#0ab8b6",
+		                "#4690cd",
+		                "#3853a4",
+		                "#5a51a2",
+		                "#63459d",
+		                "#462c83"
 		            ]
 		        }]
 			};
@@ -53,7 +61,7 @@
 
 		fn.initLine = function(){
 			var data = {
-	        labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+	        labels: ["6AM", "9AM", "12PM", "3PM", "6PM", "9PM", "12AM"],
 	        datasets: [{
 	            label: "Ph Level of Water",
 	            fillColor: "rgba(220,220,220,0.2)",
@@ -78,6 +86,36 @@
 		    type: 'line',
 		    data: data,
 		    options: option
+			});
+		}
+
+		fn.initTemp = function(){
+			var dta = {
+	        labels: ["6AM", "9AM", "12PM", "3PM", "6PM", "9PM", "12AM"],
+	        datasets: [{
+	            label: "Temperature of Water",
+	            fillColor: "rgba(220,220,220,0.2)",
+	            borderColor: "#F44336",
+	            pointBackgroundColor: "#D32F2F",
+	            pointBorderColor: "#fff",
+	            pointHoverBackgroundColor:"#FF5252",
+	            data: [36, 23, 16, 30, 32, 29, 25],
+	            lineTension:1
+	        }]
+	    };
+
+	    var opt = {
+	        responsive: true,
+	        hover: {
+            mode: 'label'
+	        }
+	    };
+	    // Get the context of the canvas element we want to select
+	    var ctx2 = d.getElementById("myTemp").getContext('2d');
+	    var myTempChart = new Chart(ctx2, {
+		    type: 'line',
+		    data: dta,
+		    options: opt
 			});
 		}
 
